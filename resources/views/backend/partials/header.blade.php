a6023c8a73fc4e26ab17e387dbdc9392c74f8d2c
Raihannoob <raihan.tanvir961@gmail.com>
2024-04-26 11:28:57 +0600

    logo change

--- resources/views/backend/partials/header.blade.php
+++ resources/views/backend/partials/header.blade.php
@@ -1,17 +1,17 @@
 <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex align-items-top flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
         <div class="me-3">
-            <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-bs-toggle="minimize">
+            {{-- <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-bs-toggle="minimize">
                 <i class="bi bi-toggles2"></i>
-            </button>
+            </button> --}}
         </div>
         <div>
             <a class="navbar-brand brand-logo" href="#">
-                <img src="https://demo.bootstrapdash.com/star-admin-2-pro/themes/assets/images/logo.svg"
+                <img src="{{ asset('backend/images/dashboard/logo.png') }}"
                     alt="logo" />
             </a>
             <a class="navbar-brand brand-logo-mini" href="#">
-                <img src="https://demo.bootstrapdash.com/star-admin-2-pro/themes/assets/images/logo.svg"
+                <img src="{{ asset('backend/images/dashboard/logo.png') }}"
                     alt="logo" />
             </a>
         </div>
